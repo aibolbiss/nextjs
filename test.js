832fbe76f0b1524e45c31c0c74b53488ced7ff8d
a.bisenkul <a.bisenkul@notamedia.ru>
2023-03-04 23:56:15 +0600

    Базовые типы

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 "use strict";
-const a = 'Aibol';
-let arr = [1, 2, 3, 4, 5];
+let a = 5;
+let b = 'Aibol';
+let c = true;
+let d = ['Aibol', 'Erlan', 'Azamat'];
+d = d.map((item) => item.toUpperCase()); // AIBOL, ERLAN, AZAMAT
+let e = 5;
+e = 'Arman';
+function Test(a, b) {
+    return a + ' ' + b;
+}
+Test('Miras', 'Kaidarov'); // Miras Kaidarov
+const Test2 = (a) => {
+    return a * 2;
+};
+Test2(3); // 6
--- test.ts
+++ test.ts
@@ -1,3 +1,24 @@
-const a: string = 'Aibol'
+let a: number = 5
 
-let arr: Array<number> = [1,2,3,4,5]
+let b: string = 'Aibol'
+
+let c: boolean = true
+
+let d: string[] = ['Aibol', 'Erlan', 'Azamat']
+
+d = d.map((item: string) => item.toUpperCase()) // AIBOL, ERLAN, AZAMAT
+
+let e: any = 5
+e = 'Arman'
+
+function Test(a:string, b: string): string | number {
+	return a + ' ' + b
+}
+
+Test('Miras', 'Kaidarov') // Miras Kaidarov
+
+const Test2 = (a:number): number => {
+	return a * 2
+}
+
+Test2(3) // 6
