d4f017831d8add3defadc474f68dc3aded3d6fc6
a.bisenkul <a.bisenkul@notamedia.ru>
2023-03-05 18:07:28 +0600

    JSX

--- test.js
+++ test.js
@@ -1,20 +1,7 @@
 "use strict";
-var Direction1;
-(function (Direction1) {
-    Direction1[Direction1["Up"] = 0] = "Up";
-    Direction1[Direction1["Down"] = 1] = "Down";
-    Direction1[Direction1["Right"] = 2] = "Right";
-    Direction1[Direction1["Left"] = 3] = "Left";
-})(Direction1 || (Direction1 = {}));
-var Direction2;
-(function (Direction2) {
-    Direction2["Up"] = "UP";
-    Direction2["Down"] = "DOWN";
-    Direction2["Right"] = "RIGHT";
-    Direction2["LEft"] = "LEFT";
-})(Direction2 || (Direction2 = {}));
-function runEnum(obj) {
-    return obj.Up;
-}
-let text = runEnum(Direction2);
-console.log(text); // UP
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const a = react_1.default.createElement("div", { className: 'react' }, "Hello React");
---
+++ test.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+const a: JSX.Element = <div className='react'>Hello React</div>
